cb2ca7121f43e50f313528edb3e0fb4477bb9e21
Maxim VT <maxim.vt@gmail.com>
2018-09-10 13:55:24 +0300

    HomeWork_MVC1_2

--- HomeWorkMVC1_2/Startup.cs
+++ HomeWorkMVC1_2/Startup.cs
@@ -5,16 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HomeWorkMVC1_2
 {
     public class Startup
     {
+        /// <summary>
+        /// Добавляем свойство для доступа к конфигурации
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Добавляем новый конструктор, принимающий интерфейс IConfiguration
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Добавляем сервисы, необходимые для mvc
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +42,15 @@ namespace HomeWorkMVC1_2
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            //Добавляем расширение для использования статических файлов, т.к. appsettings.json - это статический файл
+            app.UseStaticFiles();
+
+            //Добавляем обработку запросов в mvc-формате
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
